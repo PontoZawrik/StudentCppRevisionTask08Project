93b044c03d9f5a68dcc8cc1b8dd93c73ca531257
rasslabsya <artyomlordik@gmail.com>
2023-05-04 17:07:59 +0300

    task04

--- StudentCppRevisionTask08Project/task04.cpp
+++ StudentCppRevisionTask08Project/task04.cpp
@@ -31,7 +31,7 @@
  */
 
 bool task04(int year) {
-	if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0) {
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
 		return true;
 	}
 	return false;
